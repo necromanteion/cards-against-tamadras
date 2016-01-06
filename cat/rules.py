3781c28949dfb06fda9210911e568de10d6f7f04
necromanteion <wmmedlar@gmail.com>
2016-01-06 16:13:56 -0600

    began Rules class

--- cat/rules.py
+++ cat/rules.py
@@ -4,5 +4,22 @@ cat.rules
 This submodule contains subclasses of cat.structures.Rules -- implementations of various rules of play for the game.
 """
 
-from .structures import Rules
+import abc
 
+# from .structures import Rules
+
+
+class Rules(metaclass=abc.ABCMeta):
+    
+    
+    @abc.abstractmethod
+    def __init__(self):
+        pass
+    
+    
+    @abc.abstractmethod
+    def new_czar(self, players):
+        """Called at the beginning of a round to pick a new Card Czar."""
+        pass
+    
+    
